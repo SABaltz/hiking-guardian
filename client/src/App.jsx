e9a57b2ee126ab46699814a652fcde5c41fa4862
sam <samuelabaltz@gmail.com>
2024-09-21 21:07:24 -0800

    Update Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,10 @@
 import NavBar from "./components/NavBar.jsx";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Login from "./components/Login.jsx";
+import Register from "./components/Register.jsx";
+import About from "./components/About.jsx";
+import Home from "./components/Home.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
     // const response = await fetch('http://localhost:3001/api/test-users', {
@@ -11,11 +15,12 @@ function App() {
                 {/*<Link to="/">Home</Link>*/}
                 <Routes>
                     <Route path="/login" element={<Login/>}/>
-                    <Route path="/register" element={<Login/>}/>
-                    <Route path="/home" element={<Login/>}/>
-                    <Route path="/about" element={<Login/>}/>
+                    <Route path="/register" element={<Register/>}/>
+                    <Route path="/home" element={<Home/>}/>
+                    <Route path="/about" element={<About/>}/>
                 </Routes>
             </Router>
+            <Footer/>
         </>
     )
 }
